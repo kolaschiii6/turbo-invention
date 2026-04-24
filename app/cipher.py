af29ebdad387137df3a969e53ef8a7447b9b2b7f
zorianalesyk <lesikzorana34@gmail.com>
2026-04-24 19:15:12 +0200

    Update cipher.py

--- app/cipher.py
+++ app/cipher.py
@@ -11,14 +11,24 @@ def caesar_cipher(text, shift, mode="encrypt"):
         else:
             result += char
     return result
-# cipher module
-def rot13_cipher(text):
+def rot13_encrypt(text):
     result = ""
     for char in text:
         if char.isalpha():
             base = ord('a') if char.islower() else ord('A')
             position = ord(char) - base
             new_position = (position + 13) % 26
+            result += chr(base + new_position)
+        else:
+            result += char
+    return result
+def rot13_decrypt(text):
+    result = ""
+    for char in text:
+        if char.isalpha():
+            base = ord('a') if char.islower() else ord('A')
+            position = ord(char) - base
+            new_position = (position - 13) % 26
             result += chr(base + new_position)
         else:
             result += char
